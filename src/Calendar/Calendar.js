28a9d21346760a28e56c0af41c1c77cde4407cab
Bogdan Bikbulatov <batmansila1234@gmail.com>
2024-09-25 13:21:48 +0300

    update

--- src/Calendar/Calendar.js
+++ src/Calendar/Calendar.js
@@ -18,9 +18,57 @@ export default function Calendar() {
         document.body.style.backgroundColor = color;
     },[color])
 
+    function setupColorChange(textClasses) {
+        const getContrastYIQ = (hexcolor) => {
+            const r = parseInt(hexcolor.substr(1, 2), 16);
+            const g = parseInt(hexcolor.substr(3, 2), 16);
+            const b = parseInt(hexcolor.substr(5, 2), 16);
+            const yiq = (r * 299 + g * 587 + b * 114) / 1000;
+            return (yiq >= 128) ? 'black' : 'white';
+        };
+
+        const updateTextColor = () => {
+            const bodyBackgroundColor = window.getComputedStyle(document.body).backgroundColor;
+            const rgbValues = bodyBackgroundColor.match(/\d+/g);
+
+            if (rgbValues) {
+                const hexColor = `#${((1 << 24) + (parseInt(rgbValues[0]) << 16) + (parseInt(rgbValues[1]) << 8) + parseInt(rgbValues[2])).toString(16).slice(1)}`;
+                const textColor = getContrastYIQ(hexColor);
+
+                textClasses.forEach(className => {
+                    const elements = document.getElementsByClassName(className);
+                    for (const element of elements) {
+                        element.style.color = textColor;
+                    }
+                });
+            }
+        };
+        window.addEventListener('load', updateTextColor);
+        window.addEventListener('resize', updateTextColor);
+        window.addEventListener('click', updateTextColor);
+        const observer = new MutationObserver(updateTextColor);
+        observer.observe(document.body, {
+            attributes: true,
+            attributeFilter: ['style']
+        });
+
+        return () => {
+            window.removeEventListener('load', updateTextColor);
+            window.removeEventListener('resize', updateTextColor);
+            window.removeEventListener('click', updateTextColor);
+            observer.disconnect();
+        };
+    }
+
+    useEffect(() => {
+        setupColorChange(['FVj2te']);
+
+    },[])
+
 
     return (
         <div className="container calendar-app">
+            <p className="test">fhdso fhdsjf hdsjf hdsjkf hdsjkf </p>
             <div className="top-content">
                 <i className="fas fa-settings" onClick={() => {document.getElementById('settings').classList.toggle('show')}}><SettingsIcon backgroundColor={color}/></i>
                 <div className="settings show   " id="settings">
